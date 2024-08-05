e77cbf0ba191b3f43b1cad9d83ddd2bfd49a43e3
AlbertChoe <albert.choe73@gmail.com>
2024-08-05 17:22:34 +0700

    fix: validation for enroll

--- service/enrollment_service.go
+++ service/enrollment_service.go
@@ -21,13 +21,38 @@ func EnrollUser(db *gorm.DB, userID string, courseID int) (*models.Enrollment, e
 		return nil, fmt.Errorf("user is already enrolled in the course")
 	}
 
+	var course models.Course
+	if err := db.Where("course_id = ?", courseID).First(&course).Error; err != nil {
+		return nil, err
+	}
+
+	balance, err := GetUserBalance(db, userUUID)
+	if err != nil {
+		return nil, err
+	}
+	if balance < float64(course.Price) {
+		return nil, fmt.Errorf("insufficient balance to enroll in the course")
+	}
+
+	// Start a transaction
+	tx := db.Begin()
+
+	if err := UpdateUserBalance(tx, userUUID, -float64(course.Price)); err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
 	enrollment := &models.Enrollment{
 		UserID:       userUUID,
 		CourseID:     courseID,
 		DateEnrolled: time.Now(),
 	}
-	if err := db.Create(enrollment).Error; err != nil {
+	if err := tx.Create(enrollment).Error; err != nil {
+		tx.Rollback()
 		return nil, err
 	}
+
+	tx.Commit()
+
 	return enrollment, nil
 }
