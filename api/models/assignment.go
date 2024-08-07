14154388dcc0bc69b3b686fee8eefda46c8d0ab1
Dimas Andrian <dimas@gnuweeb.org>
2024-08-07 10:04:49 +0800

    Fix Schema

--- api/models/assignment.go
+++ api/models/assignment.go
@@ -1,10 +1,11 @@
 package models
 
 type Assignment struct {
-	AssignmentID int          `gorm:"primaryKey;autoIncrement"`
-	SyllabusID   int          `gorm:"type:int;not null"`
-	Title        string       `gorm:"type:varchar(255)"`
-	Description  string       `gorm:"type:text"`
-	MaximumTime  int          `gorm:"type:int"`
-	Submissions  []Submission `gorm:"foreignKey:AssignmentID;constraint:OnDelete:CASCADE"`
+	AssignmentID    int              `gorm:"primaryKey;autoIncrement"`
+	SyllabusID      int              `gorm:"type:int;not null"`
+	Title           string           `gorm:"type:varchar(255)"`
+	Description     string           `gorm:"type:text"`
+	MaximumTime     int              `gorm:"type:int"`
+	Submissions     []Submission     `gorm:"foreignKey:AssignmentID;constraint:OnDelete:CASCADE"`
+	UserAssignments []UserAssignment `gorm:"foreignKey:AssignmentID;constraint:OnDelete:CASCADE"`
 }
